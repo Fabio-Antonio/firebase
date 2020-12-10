a3a0b3459921b392a777b931ac6281eccf8df37c
Fabio-Antonio <ing.fabio.a@gmail.com>
2020-12-09 19:32:21 -0600

    commit v2

--- app/src/main/java/com/example/firebase/contacto.java
+++ app/src/main/java/com/example/firebase/contacto.java
@@ -37,8 +37,6 @@ public class contacto extends AppCompatActivity {
         lview.setAdapter(myadapter);
 
 
-
-
         lview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
